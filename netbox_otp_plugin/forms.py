9f7a0f3be0037fea9876b7ad192d1019ac50b436
Andrey Shalashov <avshalashov@yandex.ru>
2024-08-17 12:41:58 +0300

    forms: use get_plugin_config

--- netbox_otp_plugin/forms.py
+++ netbox_otp_plugin/forms.py
@@ -1,30 +1,40 @@
 from django import forms
-from django.conf import settings
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _
 from django_otp.forms import OTPAuthenticationFormMixin
 from django_otp import user_has_device
 
 from netbox.forms import NetBoxModelForm
+from netbox.plugins import get_plugin_config
 from users.models import User
-from utilities.forms.fields import (
-    DynamicModelChoiceField,
-)
+from utilities.forms.fields import DynamicModelChoiceField
 
 from .models import Device
 
 
 class OTPAuthenticationForm(OTPAuthenticationFormMixin, AuthenticationForm):
-    otp_device = forms.CharField(required=False, widget=forms.Select)
-    otp_token = forms.CharField(required=False, widget=forms.TextInput(attrs={'autocomplete': 'off', 'class': 'rounded'}), label="OTP Token")
-    otp_challenge = forms.CharField(required=False)
+    otp_device = forms.CharField(
+        required=False,
+        widget=forms.Select
+    )
+    otp_token = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'autocomplete': 'off',
+                'class': 'rounded'
+            }
+        ),
+        label="OTP Token"
+    )
+    otp_challenge = forms.CharField(
+        required=False
+    )
 
     def clean(self):
         self.cleaned_data = super().clean()
         user = self.get_user()
-        # use get_plugin_config from netbox.plugins better
-        plugin_config = settings.PLUGINS_CONFIG['netbox_otp_plugin']
-        otp_required = plugin_config.get('otp_required')
+        otp_required = get_plugin_config('netbox_otp_plugin', 'otp_required')
         if user_has_device(user) or otp_required:
             self.clean_otp(self.get_user())
 

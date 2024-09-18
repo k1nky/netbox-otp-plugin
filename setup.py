0dbe6628fb21aa339f6454cb9a7382bd9d0696a7
Andrey Shalashov <avshalashov@yandex.ru>
2024-09-18 22:25:50 +0300

    setup.py: set long_description_content_type

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url='https://github.com/k1nky/netbox-otp-plugin',
     author='Andrey Shalashov',
     author_email='avshalashov@yandex.ru',
+    long_description_content_type='text/markdown',
     license='Apache 2.0',
     keywords='netbox otp login plugin',
     install_requires=[
